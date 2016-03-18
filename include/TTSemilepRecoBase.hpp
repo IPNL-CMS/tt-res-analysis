2353b5b08db5421103857e50f591ad7e79a1e907
Andrey Popov <rainfinder@gmail.com>
2016-03-18 18:41:31 +0100

    Add a base class for jet assignment in semileptonic ttbar events

---
+++ include/TTSemilepRecoBase.hpp
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <PECFwk/core/AnalysisPlugin.hpp>
+
+#include <PECFwk/core/PhysicsObjects.hpp>
+
+#include <limits>
+#include <string>
+#include <vector>
+
+
+class JetMETReader;
+
+
+/**
+ * \class TTSemilepRecoBase
+ * \brief An abstract base class to perform jet assignment in a semileptonic ttbar event
+ * 
+ * This is a base class for a plugin to identify reconstructed jets corresponding to the four
+ * quarks in the final state of tt -> blv bqq. The plugin implements looping over all possible ways
+ * to assign four reconstructed jets to the four quarks, or interpretations. A derived class must
+ * implement method to compute the rank of an interpretation. The plugin accepts interpretation
+ * that gets the highest rank.
+ * 
+ * Reconstruction of the neutrino is delegated to the derived class. If multiple candidates can
+ * be reconstructed in a single event, it must choose the most suitable one.
+ * 
+ * No reconstruction is performed if an event contains less than four jets satisfying the
+ * selection. However, this plugin never rejects events.
+ */
+class TTSemilepRecoBase: public AnalysisPlugin
+{
+public:
+    /// Jets to be identified in the final state of a ttbar system
+    enum class DecayJet
+    {
+        bTopLep,   ///< Jet from semileptonically decaying top quark
+        bTopHad,   ///< Jet from hadronization of b quark from hadronically decaying top quark
+        q1TopHad,  ///< Leading light-flavour jet from hadronically decaying top quark
+        q2TopHad   ///< Subleading light-flavour jet from hadronically decaying top quark
+    };
+    
+public:
+    /**
+     * \brief Constructs a new plugin with the given name
+     * 
+     * User is encouraged to keep the default name.
+     */
+    TTSemilepRecoBase(std::string name = "TTReco");
+    
+    /// Default move constructor
+    TTSemilepRecoBase(TTSemilepRecoBase &&) = default;
+    
+    /// Assignment operator is deleted
+    TTSemilepRecoBase &operator=(TTSemilepRecoBase const &) = delete;
+    
+    /// Trivial destructor
+    virtual ~TTSemilepRecoBase() noexcept;
+    
+protected:
+    /**
+     * \brief Copy constructor
+     * 
+     * Can only be called before processing of the first dataset has started.
+     */
+    TTSemilepRecoBase(TTSemilepRecoBase const &src) noexcept;
+    
+public:
+    /**
+     * \brief Saves pointer to jet reader
+     * 
+     * User may override this method, but in this case the overriding method must initialize the
+     * pointer jetmetPlugin (possibly by calling method of the base class).
+     * 
+     * Reimplemented from Plugin.
+     */
+    void BeginRun(Dataset const &) override;
+    
+    /**
+     * \brief Returns jet corresponding to the given quark in the final state tt -> blv bqq
+     * 
+     * The behaviour is undefined if reconstruction has failed.
+     */
+    Jet const &GetJet(DecayJet type) const;
+    
+    /**
+     * \brief Returns rank of the accepted interpretation of the current event
+     * 
+     * If reconstruction has failed, returns -infinity.
+     */
+    double GetRank() const;
+    
+    /// A pure virual method to return reconstructed neutrino from the t->blv decay
+    virtual Candidate const &GetNeutrino() const = 0;
+    
+    /**
+     * \brief Sets jet selection
+     * 
+     * Only jets satisfying this selection are tried as decay products of the top quarks.
+     */
+    void SetJetSelection(double minPt, double maxAbsEta = std::numeric_limits<double>::infinity());
+    
+private:
+    /// A pure virtual method to calculate rank of a given interpretation of the current event
+    virtual double ComputeRank(Jet const &bTopLep, Jet const &bTopHad, Jet const &q1TopHad,
+      Jet const &q2TopHad) = 0;
+    
+    /**
+     * \brief Performs reconstruction of the current event
+     * 
+     * Considers all possible ways to choose four reconsted jets and match them to decay products
+     * of a pair of top quarks. Jets must satisfy the selection on pt and |eta|. For each possible
+     * interpretation its rank is computed with method ComputeRank, and the one with the highest
+     * rank is accepted.
+     * 
+     * If the event contain less than for jets satisfying the selection, reconstruction is not
+     * performed. However, the method always returns true.
+     * 
+     * User may override this method, for instance, to read additional information from the
+     * event, but the method of the base class must be called.
+     * 
+     * Implemented from Plugin.
+     */
+    virtual bool ProcessEvent() override;
+    
+protected:
+    /// Name of the plugin that produces MET
+    std::string jetmetPluginName;
+    
+    /// Non-owning pointer to the plugin that produces MET
+    JetMETReader const *jetmetPlugin;
+    
+    /**
+     * \brief Rank of the best interpretation constructed so far
+     * 
+     * When reconstruction of a new event starts, this variable is reset to -infinity. After all
+     * interpretations in an event have been considered, it contains the rank of the best
+     * interpretation.
+     */
+    double highestRank;
+    
+private:
+    /// Selection on jet transverse momentum
+    double minPt;
+    
+    /// Selection on absolute value of jet pseudorapidity
+    double maxAbsEta;
+    
+    /**
+     * \brief Non-owning pointer to input collection of jets
+     * 
+     * Needed to allow user to access identified jets.
+     */
+    std::vector<Jet> const *jets;
+    
+    /**
+     * \brief Indices of jets that pass the selection on pt and |eta|
+     * 
+     * This vector is only used in the method ProcessEvent but placed here to avoid reallocation
+     * of memory for each event.
+     */
+    std::vector<unsigned> selectedJetIndices;
+    
+    /**
+     * \brief Indices of jets identified as decay products of the top quarks
+     * 
+     * Indices correspond to collection *jets.
+     */
+    unsigned iBTopLep, iBTopHad, iQ1TopHad, iQ2TopHad;
+};
---
+++ src/TTSemilepRecoBase.cpp
@@ -0,0 +1,138 @@
+#include <TTSemilepRecoBase.hpp>
+
+#include <PECFwk/core/JetMETReader.hpp>
+#include <PECFwk/core/Processor.hpp>
+
+#include <cmath>
+#include <stdexcept>
+
+
+TTSemilepRecoBase::TTSemilepRecoBase(std::string name /*= "TTReco"*/):
+    AnalysisPlugin(name),
+    minPt(0.), maxAbsEta(std::numeric_limits<double>::infinity())
+{}
+
+
+TTSemilepRecoBase::~TTSemilepRecoBase() noexcept
+{}
+
+
+TTSemilepRecoBase::TTSemilepRecoBase(TTSemilepRecoBase const &src) noexcept:
+    AnalysisPlugin(src),
+    jetmetPluginName(src.jetmetPluginName), jetmetPlugin(nullptr),
+    minPt(src.minPt), maxAbsEta(src.maxAbsEta)
+{}
+
+
+void TTSemilepRecoBase::BeginRun(Dataset const &)
+{
+    // Save pointer to jet reader
+    jetmetPlugin = dynamic_cast<JetMETReader const *>(
+      GetMaster().GetPluginBefore(jetmetPluginName, GetName()));
+}
+
+
+Jet const &TTSemilepRecoBase::GetJet(DecayJet type) const
+{
+    switch (type)
+    {
+        case DecayJet::bTopLep:
+            return jets->at(iBTopLep);
+        
+        case DecayJet::bTopHad:
+            return jets->at(iBTopHad);
+        
+        case DecayJet::q1TopHad:
+            return jets->at(iQ1TopHad);
+        
+        case DecayJet::q2TopHad:
+            return jets->at(iQ2TopHad);
+    }
+    
+    // This must never happen
+    throw std::runtime_error("TTSemilepRecoBase::GetJet: Unhandled jet type.");
+}
+
+
+double TTSemilepRecoBase::GetRank() const
+{
+    return highestRank;
+}
+
+
+void TTSemilepRecoBase::SetJetSelection(double minPt_,
+  double maxAbsEta_ /*= std::numeric_limits<double>::infinity()*/)
+{
+    minPt = minPt_;
+    maxAbsEta = maxAbsEta_;
+}
+
+
+bool TTSemilepRecoBase::ProcessEvent()
+{
+    // Obtain collection of jets and apply the selection to it
+    jets = &jetmetPlugin->GetJets();
+    selectedJetIndices.clear();
+    
+    for (unsigned i = 0; i < jets->size(); ++i)
+    {
+        if (std::abs(jets->at(i).Eta()) > maxAbsEta)
+            continue;
+        
+        if (jets->at(i).Pt() < minPt)
+            break;  // The jet collection is ordered in pt
+        
+        selectedJetIndices.push_back(i);
+    }
+    
+    unsigned const nSelectedJets = selectedJetIndices.size();
+    
+    
+    // Do not attempt reconstruction if there is not enough jets. However, do not reject the event
+    if (selectedJetIndices.size() < 4)
+        return true;
+    
+    
+    // Loop over all possible ways of jet assignment to find the best one
+    highestRank = -std::numeric_limits<double>::infinity();
+    iBTopLep = iBTopHad = iQ1TopHad = iQ2TopHad = -1;
+    
+    for (unsigned iiBTopLepCand = 0; iiBTopLepCand < nSelectedJets; ++iiBTopLepCand)
+        for (unsigned iiBTopHadCand = 0; iiBTopHadCand < nSelectedJets; ++iiBTopHadCand)
+        {
+            if (iiBTopLepCand == iiBTopHadCand)
+                continue;
+            
+            for (unsigned iiQ1TopHadCand = 0; iiQ1TopHadCand < nSelectedJets; ++iiQ1TopHadCand)
+            {
+                if (iiQ1TopHadCand == iiBTopLepCand or iiQ1TopHadCand == iiBTopHadCand)
+                    continue;
+                
+                // When looping for the subleading light-flavour jet, take into account that the
+                //collection is still ordered in jet pt
+                for (unsigned iiQ2TopHadCand = iiQ1TopHadCand + 1; iiQ2TopHadCand < nSelectedJets;
+                  ++iiQ2TopHadCand)
+                {
+                    // An interpretation has been constructed. Evaluate it
+                    double const rank = ComputeRank(jets->at(selectedJetIndices.at(iiBTopLepCand)),
+                      jets->at(selectedJetIndices.at(iiBTopHadCand)),
+                      jets->at(selectedJetIndices.at(iiQ1TopHadCand)),
+                      jets->at(selectedJetIndices.at(iiQ2TopHadCand)));
+                    
+                    if (rank > highestRank)
+                    {
+                        highestRank = rank;
+                        
+                        iBTopLep = selectedJetIndices.at(iiBTopLepCand);
+                        iBTopHad = selectedJetIndices.at(iiBTopHadCand);
+                        iQ1TopHad = selectedJetIndices.at(iiQ1TopHadCand);
+                        iQ2TopHad = selectedJetIndices.at(iiQ2TopHadCand);
+                    }
+                }
+            }
+        }
+    
+    
+    // Always return true since this plugin does not perform event filtering
+    return true;
+}
